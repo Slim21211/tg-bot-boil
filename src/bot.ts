f0cb8bef3aa39ccac04610eef6261ebbfb15c11f
Vladimir Davydov <v.g.davydoff@gmail.com>
2025-05-26 08:05:50 +0300

    dev mode

--- src/bot.ts
+++ src/bot.ts
@@ -3,10 +3,21 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-if (!process.env.BOT_TOKEN) {
+const token = process.env.BOT_TOKEN;
+const mode = process.env.MODE || 'production';
+
+if (!token) {
   throw new Error('BOT_TOKEN не найден');
 }
 
-export const bot = new Telegraf(process.env.BOT_TOKEN);
+export const bot = new Telegraf(token);
 
-bot.start((ctx) => ctx.reply('Привет! Я бот! И точка! '));
+bot.start((ctx) => ctx.reply('Привет! Я бот!'));
+
+if (mode === 'local') {
+  bot.launch();
+  console.log('Бот запущен в режиме polling');
+
+  process.once('SIGINT', () => bot.stop('SIGINT'));
+  process.once('SIGTERM', () => bot.stop('SIGTERM'));
+}
